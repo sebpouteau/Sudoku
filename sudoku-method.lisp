2821e995b62975b5e5f33e27fabdb4d73f522e9e
jimgouraud <jimmy.gouraud@etu.u-bordeaux.fr>
2015-10-21 13:28:38 +0200

    changement affiche sudoku

--- sudoku-method.lisp
+++ sudoku-method.lisp
@@ -1,4 +1,3 @@
-
 					;===========================================
 					;==           Implémentation              ==
 					;==                                       ==
@@ -50,19 +49,97 @@
 	  (x-coor (coor (aref (squares-array squares) x y )))
 	  (y-coor (coor (aref (squares-array squares) x y))))))
 
+;; Affiche le jeu
+(defun printgrid (squares)
+  ; Affiche le titre
+  (printTitle)
 
-(defun printgrid (squares)    
-    (loop for y from 0 to (1- *size*)
+  ; Affiche la barre au dessus des lettres
+  (printBar "    ┌" "┬" "┐")
+
+  ; Affiche les lettres des colonnes
+  (princ "    |")
+  (loop for cpt from 0 to (1- *size*) do
+       (princ " ")
+       (princ (code-char (+ cpt 65)))
+       (if (eq (mod (1+ cpt) *sqrt-size*) 0)
+	   (princ " |")
+	   (princ " ")))
+  (terpri)
+
+  ; Affiche la barre en dessous des lettres
+  (printBar "┌───┼" "┤" "┤")
+
+  ; Affiche les chiffres des lignes, ainsi que la grille
+  (loop for y from 0 to (1- *size*)
+     do
+       (princ "| ")
+       (princ (1+ y))
+       (princ " |")
+       (loop for x from 0 to (1- *size*)
 	  do
-	     (let ((list-line '()))
-	       (loop for x from 0 to (1- *size*)
-		   do
-		      (setf list-line
-			    (cons
-			     (digit (aref (squares-array squares) x y))
-			     list-line )))
-	       (print (reverse list-line)))
-	  ))
+	    (princ " ")
+	    (princ (digit (aref (squares-array squares) x y)))
+	    (if (eq (mod (1+ x) *sqrt-size*) 0)
+		(princ " |")
+		(princ " "))
+	    )
+       (terpri)
+
+       (when (and (eq (mod (1+ y) *sqrt-size*) 0)
+		(not (eq y (1- *size*))))
+	 ; Affiche les barres séparant les zones
+	 (printBar "├───┼" "┼" "┤")
+	 ))
+
+  ; Affiche la barre de fin
+  (printBar "└───┴" "┴" "┘")
+
+
+  (defparameter line 0)
+  (defparameter column 0)
+  (askCase 'line 'column)
+)
+
+(defun printTitle ()
+  (princ "
+ ╔═══╗ ╔   ╗ ╔══╗ ╔═══╗ ╔ ╔═ ╔   ╗
+ ╚═══╗ ║   ║    ║ ║   ║ ╠═╣  ║   ║
+ ╚═══╝ ╚═══╝ ╚══╝ ╚═══╝ ╚ ╚═ ╚═══╝
+
+"))
+
+(defun printBar (debut milieu fin)
+  (princ debut)
+  (loop for cpt from 0 to (1- *size*) do
+       (princ "───")
+       (when (and (eq (mod (1+ cpt) *sqrt-size*) 0)
+		  (not (eq (1+ cpt) *size*)))
+	 (princ milieu)))
+  (princ fin)
+  (terpri))
+
+(defun askCase(line column)
+  (terpri)
+  (princ "Enter the column letter: ")
+  (setf column(read))
+  (loop while (not (or (<= 65 (char-code #'column) 90)
+		       (<= 97 (char-code #'column) 122))) do
+       (princ "You need to enter a letter. Try Again : ")
+       (setf column(read)))
+
+  (princ "Enter the line number: ")
+  (setq line(read))
+  (loop while (not (numberp line)) do
+       (princ "You need to enter a number. Try Again : ")
+       (setq line(read)))
+)
+
+
+
+
+       
+
 
 ;; ==================
 ;; ==     Game     ==
