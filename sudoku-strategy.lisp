96b574e726c238cc3044ea5ca7ebd447b3e1a883
sebastien <sebastien.pouteau@etu.u-bordeaux.fr>
2015-11-02 17:44:48 +0100

    modification strategy ajout verif subsquare

--- sudoku-strategy.lisp
+++ sudoku-strategy.lisp
@@ -6,31 +6,49 @@
 
 (in-package :sudoku)
 
+(defvar x nil)
+(defvar y nil)
 (defmethod strategy (game)
-(block fin
-  ;; Test si il existe un carré n'ayant qu'une possibilité
-  (loop for y from 0 to (1- *size*) do
-       (loop for x from 0 to (1- *size*) do
-	    (when (not (protected ( aref (squares-array (game-squares game)) x y)))
-	      (when (= 1 (length (get-possibility game x y)))
-		(return-from fin (values x y (car (get-possibility game x y))))))))
-  ;; test plus approfondit
-  (loop for y from 0 to (1- *size*) do
-       (loop for x from 0 to (1- *size*) do
-		 
-	       
+  (update-possibility-all-square (game-squares game))
+  (block fin
+    ;; Test si il existe un carré n'ayant qu'une possibilité
+    ;(loop for y from 0 to (1- *size*) do
+      ;(loop for x from 0 to (1- *size*) do
+;	(when (and (not (protected ( aref (squares-array (game-squares game)) x y)))
+	;	   (= 1 (length (get-possibility game x y))))
+	;  (return-from fin (values x y (car (get-possibility game x y)))))))
+    
+    ;; test plus approfondit
+    ;(loop for y from 0 to (1- *size*) do
+       ;(loop for x from 0 to (1- *size*) do
+	 ;(when (not (protected ( aref (squares-array (game-squares game)) x y)))
 
-))))
+    (loop for y from 0 to (1- *size*) do
+      (loop for x from 0 to (1- *size*) do
+	(let ((list (verifier-subsquare game x y )))
+	  (if (= (length list) 1)
+	      (return-from fin (values x y (car list))))
+      
+    
+    )))))
 
-(defun verifier-subsquare(game x y)
-   (let ((list '())
-	(array (squares-array squares))
+ 
+(defun verifier-subsquare(game coorX coorY)
+   (let ((list (get-possibility game coorX coorY))
+	
 	;; definition des débuts et fins du petit carré qui contient square (x,y)
-	(departX (* (truncate (/ x *sqrt-size*)) *sqrt-size*))
-	(finX (- (* (1+ (truncate (/ x *sqrt-size*))) *sqrt-size*) 1)) 
-	(departY (* (truncate (/ y *sqrt-size*)) *sqrt-size*))
-	(finY (- (* (1+ (truncate (/ y *sqrt-size*))) *sqrt-size*) 1)) )
+	(departX (* (truncate (/ coorX *sqrt-size*)) *sqrt-size*))
+	(finX (- (* (1+ (truncate (/ coorX *sqrt-size*))) *sqrt-size*) 1)) 
+	(departY (* (truncate (/ coorY *sqrt-size*)) *sqrt-size*))
+	(finY (- (* (1+ (truncate (/ coorY *sqrt-size*))) *sqrt-size*) 1)) )
      (loop for x from departX to finX do
-	  (loop for y from departY to finY do
-	       (append (get-possibility game x y) list)))
-     ))
+       (loop for y from departY to finY do
+	 (unless (or (and (eq x coorX)
+			  (eq y coorY))
+		     (protected ( aref (squares-array (game-squares game)) x y)))
+	   (setf list (remove-sublist (get-possibility game x y) list)))
+	 
+	     ))
+     (unless (endp list)
+       list)
+))
